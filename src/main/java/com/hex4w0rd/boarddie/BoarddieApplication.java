cc14b57cc4fd3a14fb83d9173fe664fd541afb70
Emanuel Sotomayor <esotomayor@sms-sudamerica.com>
2024-06-09 18:43:37 -0300

    Initial project

---
+++ src/main/java/com/hex4w0rd/boarddie/BoarddieApplication.java
@@ -0,0 +1,13 @@
+package com.hex4w0rd.boarddie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoarddieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoarddieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hex4w0rd/boarddie/BoarddieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hex4w0rd.boarddie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoarddieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
